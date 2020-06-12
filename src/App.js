6662ed718cdc227e4d597006e5e3fd0f6af5fb52
Kevyn Klava <kevynsax@gmail.com>
2020-06-12 14:17:05 -0300

    Correct base name on route

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Fases from "./Components/Fases";
 import Play from "./Components/Play";
 
 class App extends Component {
-  render = () => (
-    <Router>
-      <Fragment>
-      <Route exact path="" component={Fases}></Route>
-      <Route path="/Play/:level" component={Play}></Route>
-      </Fragment>
-    </Router>
-  )
+  render = () => {
+      const isProduction = process.env.NODE_ENV === 'production';
+      return (
+          <Router basename={isProduction ? '/jogo-matica' : ''}>
+              <Fragment>
+                  <Route exact path="" component={Fases}></Route>
+                  <Route path="/Play/:level" component={Play}></Route>
+              </Fragment>
+          </Router>
+      );
+  }
 }
 
 export default App;
